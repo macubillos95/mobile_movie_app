df251cafafaa6b255010ec966a36dddc37f40b75
macc <miguelmaccmiguel@gmail.com>
2026-03-22 18:32:47 -0500

    prueba 3 borrar

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,16 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-light-300 font-bold">Prueba 2</Text>
+      <Text className="text-5xl text-light-300 font-bold">Prsueba </Text>
+      <Link href="/onboarding" className="text-2xl text-light-300">
+        Go to Onboarding
+      </Link>
+      <Link href="/movie/avengers" className="text-2xl text-light-300">
+        Avenger
+      </Link>
     </View>
   );
 }
---
+++ app/onboarding.tsx
@@ -0,0 +1,15 @@
+// onboarding.tsx
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Onboarding = () => {
+  return (
+    <View>
+      <Text>Onboarding</Text>
+    </View>
+  );
+};
+
+export default Onboarding;
+
+const styles = StyleSheet.create({});
